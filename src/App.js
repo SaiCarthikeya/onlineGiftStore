8a3d36498f43991510d8d1fc8f75672230c67404
SaiCarthikeya <saicarthik180@gmail.com>
2024-06-12 22:07:47 +0530

    v0 barebone

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
+import Login from './components/Login'
+import Home from './components/Home'
+import Admin from './components/Admin'
+import About from './components/About'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login/>}></Route>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="admin" element={<Admin/>}></Route>
+        <Route path="about" element={<About/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
